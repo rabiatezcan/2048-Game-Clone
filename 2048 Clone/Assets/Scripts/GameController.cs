a968cb5cb0e8fbbb438bd4f9730d627ab3b380b8
rabiatezcan <rabiatezcan@hotmail.com>
2021-08-04 14:15:39 +0300

    feat : game controls added
    Winning, Game Over controls added.
    Score tracking added.

--- 2048 Clone/Assets/Scripts/GameController.cs
+++ 2048 Clone/Assets/Scripts/GameController.cs
@@ -1,54 +1,182 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
+    public static GameController instance;
+    public static int ticker;
+    
     [SerializeField] GameObject fillPrefab;
 
-    [SerializeField] Transform[] cells;
-    
-    void Start()
+    [SerializeField] Cell[] cells;
+    public static Action<MoveDirection> slide;
+    #region UI elements
+    public int score;
+    [SerializeField] Text scoreText;
+    private int isGameOver;
+    [SerializeField] private GameObject gameOverPanel;
+    [SerializeField] private int winningScore;
+    [SerializeField] private GameObject winningPanel;
+    private bool hasWon; 
+    #endregion
+
+    public Color[] fillColors;
+    private void OnEnable()
     {
-    }
-    
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (instance == null)
         {
-            SpawnFill();
+            instance = this;
         }
     }
 
+    void Start()
+    {
+        StartSpawnFill();
+        StartSpawnFill();
+        gameOverPanel.SetActive(false);
+        winningPanel.SetActive(false);
+    }
+
+    void Update()
+    {
+       GetInput();
+    }
+
+    public void GetInput()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            CountTicker();
+            Debug.Log("UP");
+            slide(MoveDirection.Up);
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            CountTicker();
+            Debug.Log("RIGHT");
+            slide(MoveDirection.Right);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            CountTicker();
+            Debug.Log("LEFT");
+            slide(MoveDirection.Left);
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            CountTicker();
+            Debug.Log("DOWN");
+            slide(MoveDirection.Down);
+        }
+    }
+    public void CountTicker()
+    {
+        ticker++;
+        if (ticker == 4)
+        {
+            ticker = 0; 
+            SpawnFill();
+        }
+    }
     public void SpawnFill()
     {
-        float chance = Random.Range(0f, 1f);
-        int whichSpawn = Random.Range(0, cells.Length);
-        
-        if (cells[whichSpawn].childCount == 0)
+        bool isFull = true;
+        for (int i = 0; i < cells.Length; i++)
+        {
+            if (cells[i].fill == null)
+            {
+                isFull = false;
+            }
+        }
+        if (isFull)
+        {
+            return;
+        }
+        float chance = UnityEngine.Random.Range(0f, 1f);
+        int whichSpawn = UnityEngine.Random.Range(0, cells.Length);
+        int tempFillValue = 0;
+        if (cells[whichSpawn].transform.childCount == 0)
         {
             if (chance < .2f)
             {
-                Debug.Log("bos");
                 return;
             }
             else if (chance < .8f)
             {
-                Debug.Log(2);
-                // TODO: 2 üretecek  
+                tempFillValue = 2;
             }
             else
             {
-                Debug.Log(4);
-                // TODO: 4 üretecek
+                tempFillValue = 4;
             }
-            GameObject tempFill = Instantiate(fillPrefab, cells[whichSpawn]);
+
+            GameObject tempFill = Instantiate(fillPrefab, cells[whichSpawn].transform);
+            Fill tempFillText = tempFill.GetComponent<Fill>();
+            cells[whichSpawn].GetComponent<Cell>().fill = tempFillText;
+            tempFillText.FillValueUpdate(tempFillValue);
         }
         else
         {
             SpawnFill();
             return;
         }
+
+
+    }
+
+    public void StartSpawnFill()
+    {
+        int whichSpawn = UnityEngine.Random.Range(0, cells.Length);
+        if (cells[whichSpawn].transform.childCount == 0)
+        {
+            GameObject tempFill = Instantiate(fillPrefab, cells[whichSpawn].transform);
+            Fill tempFillText = tempFill.GetComponent<Fill>();
+            cells[whichSpawn].GetComponent<Cell>().fill = tempFillText;
+            tempFillText.FillValueUpdate(2);
+        }
+        else
+        {
+            SpawnFill();
+            return;
+        }
+    }
+
+    public void ScoreUpdate(int score)
+    {
+        this.score = score;
+        scoreText.text = this.score.ToString();
+    }
+
+    public void GameOverCheck()
+    {
+        isGameOver++;
+        if (isGameOver >= 16)
+        {
+            gameOverPanel.SetActive(true);
+        }
+    }
+    public void Restart()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void WinningCheck(int highestFill)
+    {
+        if(hasWon)
+            return;
+        if (highestFill == winningScore)
+        {
+            winningPanel.SetActive(true);
+            hasWon = true;
+        }
+    }
+
+    public void KeepPlaying()
+    {
+        winningPanel.SetActive(false);
     }
 }
