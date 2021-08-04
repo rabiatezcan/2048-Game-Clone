e5513e6b0ca3c62ab549f4713ffc75ae0f3c1760
rabiatezcan <rabiatezcan@hotmail.com>
2021-08-04 14:13:15 +0300

    feat : Cell's features and moving function added.

---
+++ 2048 Clone/Assets/Scripts/Cell.cs
@@ -0,0 +1,164 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum MoveDirection
+{
+    Left,
+    Right,
+    Up,
+    Down
+}
+
+public class Cell : MonoBehaviour
+{
+    public Cell right;
+    public Cell left;
+    public Cell up;
+    public Cell down;
+
+
+    public Fill fill;
+
+    private void OnEnable()
+    {
+        GameController.slide += OnSlide;
+    }
+
+    private void OnDisable()
+    {
+        GameController.slide -= OnSlide;
+    }
+
+    private Cell ControlDirection(MoveDirection direction)
+    {
+        if (direction == MoveDirection.Down)
+            return up;
+        else if (direction == MoveDirection.Left)
+            return right;
+        else if (direction == MoveDirection.Right)
+            return left;
+        else
+            return down;
+    }
+
+    private void OnSlide(MoveDirection direction)
+    {
+        //CellCheck(); 
+        Cell currentCell = this;
+        if (direction == MoveDirection.Up)
+        {
+            if (up != null)
+                return;
+        }
+
+        if (direction == MoveDirection.Right)
+        {
+            if (right != null)
+                return;
+        }
+
+        if (direction == MoveDirection.Down)
+        {
+            if (down != null)
+                return;
+        }
+
+        if (direction == MoveDirection.Left)
+        {
+            if (left != null)
+                return;
+        }
+
+        Slide(currentCell, direction);
+    }
+
+    private void Slide(Cell currentCell, MoveDirection direction)
+    {
+        if (currentCell.ControlDirection(direction) == null)
+            return;
+
+        Cell nextCell = currentCell.ControlDirection(direction);
+        while (nextCell.ControlDirection(direction) != null && nextCell.fill == null)
+        {
+            nextCell = nextCell.ControlDirection(direction);
+        }
+        if (nextCell.fill != null)
+        {
+            if (currentCell.fill != null)
+            {
+                if (currentCell.fill.value == nextCell.fill.value)
+                {
+                    nextCell.fill.Double();
+                    nextCell.fill.transform.parent = currentCell.transform;
+                    currentCell.fill = nextCell.fill;
+                    nextCell.fill = null;
+                }
+                else if (currentCell.ControlDirection(direction).fill != nextCell.fill)
+                {
+                    nextCell.fill.transform.parent = currentCell.ControlDirection(direction).transform;
+                    currentCell.ControlDirection(direction).fill = nextCell.fill;
+                    nextCell.fill = null;
+                }
+            }
+            else
+            {
+                nextCell.fill.transform.parent = currentCell.transform;
+                currentCell.fill = nextCell.fill;
+                nextCell.fill = null;
+                Slide(currentCell, direction);
+            }
+        }
+        if (currentCell.ControlDirection(direction) == null)
+            return;
+        Slide(currentCell.ControlDirection(direction), direction);
+    }
+
+
+//     void CellCheck()
+//     {
+//         if (fill == null)
+//         {
+//             return;
+//         }
+//
+//         if (up != null)
+//         {
+//             if (up.fill == null)
+//             {
+//                 if (up.fill.value == null)
+//                     return;
+//             }
+//         }
+//
+//         if (down != null)
+//         {
+//             if (down.fill == null)
+//             {
+//                 if (down.fill.value == null)
+//                     return;
+//             }
+//         }
+//
+//         if (right != null)
+//         {
+//             if (right.fill == null)
+//             {
+//                 if (right.fill.value == null)
+//                     return;
+//             }
+//         }
+//
+//         if (left != null)
+//         {
+//             if (left.fill == null)
+//             {
+//                 if (left.fill.value == null)
+//                     return;
+//             }
+//         }
+//
+//         GameController.instance.GameOverCheck();
+//     }
+ }
